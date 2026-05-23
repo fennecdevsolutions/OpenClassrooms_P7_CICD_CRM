49a7e54bae9352323e35405776c90cc0e04a81bc
Fennec Dev <fennec.dev.solutions@gmail.com>
2026-05-23 15:17:02 +0200

    Downgrade ELK version to 7.17 for compatibilty with APM; Add frontend to ELK pipeline

--- front/src/main.ts
+++ front/src/main.ts
@@ -1,6 +1,19 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
+import { init as initApm } from '@elastic/apm-rum';
 import { AppComponent } from './app/app.component';
+import { appConfig } from './app/app.config';
+
+const apm = initApm({
+  serviceName: 'orion-frontend',
+
+  // APM container URL
+  serverUrl: 'http://localhost:8200',
+
+  // Activate HTTP requests capture for analysis
+  distributedTracingOrigins: ['http://localhost:8080']
+})
 
 bootstrapApplication(AppComponent, appConfig)
   .catch((err) => console.error(err));
+
+
